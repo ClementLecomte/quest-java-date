cfb70da7b9d3224f433df7b6d25458ddbecde356
ClementLecomte <lecomte.c49@gmail.com>
2019-07-24 10:01:04 +0100

    all done

--- src/TestDate.java
+++ src/TestDate.java
@@ -1,8 +1,10 @@
+import java.sql.SQLOutput;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.text.ParseException;
 
 class TestDate {
 
@@ -12,12 +14,20 @@ class TestDate {
         System.out.println("Please fill a day (1-31) :");
         int day = sc.nextInt();
         System.out.println("Please fill a month (1-12) :");
-        int month = sc.nextInt();
+        int month = sc.nextInt()-1;
         System.out.println("Please fill a year :");
         int year = sc.nextInt();
         sc.close();
 
         // TODO : your code after this line
-        
+        SimpleDateFormat sdf = new SimpleDateFormat("MM-dd-yyyy");
+        Calendar calendar = new GregorianCalendar(year, month, day);
+        Date date = calendar.getTime();
+        System.out.println("The date is : " + sdf.format(date));
+
+
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        System.out.println("The day is : " + dayOfWeek);
+
     }
 }
